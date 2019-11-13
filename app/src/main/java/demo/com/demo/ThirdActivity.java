c2810f9bc7781baea2f36eb776be96c5cf366e65
Gogs <gogs@fake.local>
2019-11-13 14:37:16 +0800

    V1

---
+++ app/src/main/java/demo/com/demo/ThirdActivity.java
@@ -0,0 +1,23 @@
+package demo.com.demo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 类或接口的描述信息
+ *
+ * @Author:qubin
+ * @Theme:
+ * @Data:2019-11-13
+ * @Describe:
+ */
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo);
+    }
+}
