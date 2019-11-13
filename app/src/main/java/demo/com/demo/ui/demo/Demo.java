76afad94dc17af3bcbeda9090dd0ccf242573483
Gogs <gogs@fake.local>
2019-11-13 13:58:50 +0800

    设置一个分支代码

--- app/src/main/java/demo/com/demo/ui/demo/Demo.java
+++ app/src/main/java/demo/com/demo/ui/demo/Demo.java
@@ -1,5 +1,11 @@
 package demo.com.demo.ui.demo;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import demo.com.demo.R;
+
 /**
  * 类或接口的描述信息
  *
@@ -8,5 +14,11 @@ package demo.com.demo.ui.demo;
  * @Data:2019-11-13
  * @Describe:
  */
-public class Demo {
+public class Demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo);
+    }
 }
